0844d88748a26bdcb2c889853bca3a314ccfbb1c
Mikail Kalkan <mikailkalkanpro@gmail.com>
2023-02-20 16:36:54 +0100

    Initial Commit

---
+++ Shared/CVApp.swift
@@ -0,0 +1,17 @@
+//
+//  CVApp.swift
+//  Shared
+//
+//  Created by stage on 20/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct CVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
